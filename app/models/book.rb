68ddd77f4e905b04d46f9cd750659ef69cf88377
Eduardo Serafin <e.serafin@ed.ac.uk>
2014-04-08 16:42:27 +0100

    Handling multivaried author fields

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ActiveFedora::Base
   has_metadata 'descMetadata', type: BookMetadata
 
   has_attributes :title, datastream: 'descMetadata', multiple: false
-  has_attributes :author, datastream: 'descMetadata', multiple: false
+  has_attributes :author, datastream: 'descMetadata', multiple: true
   
   # within app/models/book.rb
   has_many :pages, :property=> :is_part_of
