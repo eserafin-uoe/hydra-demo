6651a68d35ab202af89e877b42ec83f4c42a71be
Mark Bussey <mark@curationexperts.com>
2014-04-09 16:27:32 +0100

    Added publisher metada term

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ActiveFedora::Base
 
   has_attributes :title, datastream: 'descMetadata', multiple: false
   has_attributes :author, datastream: 'descMetadata', multiple: false
+  has_attributes :publisher, datastream: 'descMetadata', multiple: false
 
   has_many :pages, :property=> :is_part_of
 end
