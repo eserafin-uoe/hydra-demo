f95416f1f4109316cb14a57ea94c0d1cb740e6f4
Eduardo Serafin <e.serafin@ed.ac.uk>
2014-04-11 12:21:49 +0100

    Fixed all HEAD refernces

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,8 +9,6 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-<<<<<<< HEAD
+
 HydraDemo::Application.config.secret_key_base = '04f9d8567da867073025f76153f3aabc9504312871b294a597338ef84754e7d7848e39ad150cdff69489a08abaac3421fd0ed89e0ac0a607d7a41da5d1b68e09'
-=======
-HydraDemo::Application.config.secret_key_base = '8c0007ccebfc50e43f9c870f5844c51b4eb81b828e48a12652a8586b379ae5cc716e2701e45d7a30c90d6ff69cf63ef0528f43fb52c160942ee0c7bffad296d9'
->>>>>>> 6651a68d35ab202af89e877b42ec83f4c42a71be
+
